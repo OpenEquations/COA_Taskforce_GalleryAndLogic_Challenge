9c585177d6d0fdb60cc258c3f8c55c21f41ebeb8
joe.lebonheur <joe.lebonheur@gmail.com>
2024-06-08 18:47:37 +0200

    debugging and refining codes

--- README.md
+++ README.md
@@ -6,10 +6,23 @@ This project is an interactive photo gallery that adapts to different screen siz
 ## Features
 - Responsive design
 - Hover interaction to display details
+- Visitor can click on an image to open a preview!
+- A priview also has a thumb nairl
 - Cross-browser compatibility
 - Clean and well-structured code
 
 ## Installation
+
 1. Clone the repository:
    ```sh
    git clone https://github.com/yourusername/interactive-photo-gallery.git
+
+2. You can open _index.html_ in the root directory
+
+### Challenges 
+
+ - Open challenges/arrayMap.html
+ **Note: _open console, you will outputs_ find JS codes in arrayMap.js**
+
+  - Open challenges/stringTransform.html
+ **Note: _open console, you will outputs_ find JS codes in stringTransform.js**
--- challanges/arrayMap.js
+++ challanges/arrayMap.js
@@ -1,5 +1,6 @@
 /**
  * Finds the contiguous subarray within an array that sums to a target value.
+ * contigArr stores temporally arrays that are contiguous but is creaned whenver it found exceeding or when the loop ends with sum less than target
  */
 function findContiguousSubarray(arr, target) {
     let contigArr = []; // Array to store the contiguous subarray
@@ -10,22 +11,28 @@ function findContiguousSubarray(arr, target) {
         sum += arr[i]; // Add current element to the sum
         contigArr.push(arr[i]); // Add current element to the contiguous subarray
 
-        if (sum > target) {
+        if (target > 0 && sum > target) { // case where target is a positive number
             // If the sum exceeds the target, reset and start from the next element
             sum = 0;
-            contigArr = [];
+            contigArr = []; // store contiguous temporally
             i = ++j - 1; // Move the starting point forward and adjust i
+        } else if (target < 0 && sum < target) { // case where target is a negative number
+                        // If the sum exceeds the target, reset and start from the next element
+                        sum = 0;
+                        contigArr = [];
+                        i = ++j - 1; // Move the starting point forward and adjust i
         } else if (sum == target) {
             // If the sum equals the target, break the loop as the target is found
             break;
         }
+        if(i === arr.length - 1 && sum < target) contigArr = [];
     }
-
-    return contigArr; // Return the contiguous subarray
+    if(contigArr.length) return true //return true if contigArr is not empty
+    return false; // Return false if contigArr is empty
 }
 
 // Example usage:
-let arr = [15, 5, -2,-3, 16, 1, 2,3]; // Input array
-let target = 17; // Target sum value
+let arr = [1, 2, 3, 4, 5]; // Input array
+let target = 16; // Target sum value
 
 console.log(findContiguousSubarray(arr, target)); // Output the contiguous subarray
